72d0da70f34f22fe597766d351cd74cbad606388
Thomas <Thomas@Thomas-PC>
2014-11-27 19:31:53 +0100

    mockito

--- netwolve/src/test/java/tom/lib/netwolve/elements/NetworkTest.java
+++ netwolve/src/test/java/tom/lib/netwolve/elements/NetworkTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import tom.lib.netwolve.interfaces.Transformation;
 
@@ -12,20 +13,22 @@ public class NetworkTest {
 
 	@Test
 	public void creationTest(){
-			// Création des neurones internets
-			Transformation[] inputs = new Transformation[2];
-			Transformation[] inners = new Transformation[10];
-			for (int i = 0; i < inners.length; i++) {
-				inners[i] = new Neural(3);
-			}
-			inputs[0] = inners[0];
-			inputs[1] = inners[1];
-			Network network = new Network(inputs, inners, 2, 0.5);
+		Transformation t = Mockito.mock(Transformation.class);
+		Mockito.when(t.getLastEval()).thenReturn(new float[]{});
+		// Création des neurones internes
+		Transformation[] inputs = new Transformation[2];
+		Transformation[] inners = new Transformation[10];
+		for (int i = 0; i < inners.length; i++) {
+			inners[i] = new Neural(3);
+		}
+		inputs[0] = inners[0];
+		inputs[1] = inners[1];
+		Network network = new Network(inputs, inners, 2, 0.5);
 
-			System.out.println(Arrays.toString(network.eval(new float[]{0.1f, 0.2f})));
-			for (int i = 0; i < 3; i++) {
-				System.out.println(Arrays.toString(network.eval()));
-			}
-			assertTrue(true);
+		System.out.println(Arrays.toString(network.eval(new float[]{0.1f, 0.2f})));
+		for (int i = 0; i < 3; i++) {
+			System.out.println(Arrays.toString(network.eval()));
+		}
+		assertTrue(true);
 	}
 }
